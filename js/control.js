0133e156798b4a87fd9457f76f3f9afb2164c5bc
fish <ljd200102@163.com>
2020-01-28 19:56:27 +0800

    frist

---
+++ js/control.js
@@ -0,0 +1,54 @@
+function showdata(item){
+    //document.getElementById('').style.display
+    var list=item.nextElementSibling;
+    //alert(list.style.display);
+    if(list.style.display=='none'){
+        list.style.display='block';
+    }else{
+        list.style.display='none';
+    }
+}
+scoredata.scores=[
+    {
+        date:'2019-2020 1',
+        code:'TX870601',
+        id:'TX870601.02',
+        name:'大学生生涯规划与就业指导',
+        type:'通识选修课程创新创业与就业指导类',
+        credit:'2',
+        testScore:'95',
+        usualScore:'98',
+        overallScore:'96',
+        experimentScore:'',
+        finalScore:'96',
+        GPA:'4.6'
+    },
+    {
+        date:'2019-2020 1',
+        code:'TX870601',
+        id:'TX870601.02',
+        name:'大学生生涯规划与就业指导',
+        type:'通识选修课程创新创业与就业指导类',
+        credit:'2',
+        testScore:'95',
+        usualScore:'98',
+        overallScore:'96',
+        experimentScore:'',
+        finalScore:'96',
+        GPA:'4.6'
+    },
+    {
+        date:'2019-2020 1',
+        code:'TX870601',
+        id:'TX870601.02',
+        name:'大学生生涯规划与就业指导',
+        type:'通识选修课程创新创业与就业指导类',
+        credit:'2',
+        testScore:'95',
+        usualScore:'98',
+        overallScore:'96',
+        experimentScore:'',
+        finalScore:'96',
+        GPA:'4.6'
+    }
+];
---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload(){
+    var a=new Vue({
+        el: '#app',
+        data: {
+          sites: [
+            { name: 'Runoob' ,j:'gg'},
+            { name: 'Google' ,j:'cc'},
+            { name: 'Taobao' ,j:'aa'}
+          ]
+        }
+      });
+}
